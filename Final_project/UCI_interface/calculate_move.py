49a35ef7479212be474e428dc8d1b7c76539a618
Aditya <aditya.ramesh7@gmail.com>
2019-03-22 14:31:07 -0400

    First commit

--- Final_project/UCI_interface/calculate_move.py
+++ Final_project/UCI_interface/calculate_move.py
@@ -1,13 +1,16 @@
 import chess
-import chess.engine
+import chess.uci
 
-def next_move(board_state):
-	engine = chess.engine.SimpleEngine.popen_uci("/home/aditya1709/Documents/Michigan_courses/EECS598_HCI/Final_project/UCI_interface/chessengines/stockfish_1/Linux/stockfish_1")
+def next_move(board_state, engine_name):
+	engine = chess.uci.popen_engine("/home/aditya1709/Documents/Michigan_courses/EECS598_HCI/Final_project/UCI_interface/chessengines/"+engine_name+"/Linux/"+engine_name)
+	engine.uci()
 	board = chess.Board(board_state)
-	result = engine.play(board, chess.engine.Limit(time=0.100))
-	board.push(result.move)
+	engine.position(board)
+	result = engine.go(movetime=2000)
+	board.push(chess.Move.from_uci(str(result.bestmove.uci())[:4]))
 	engine.quit()
-	return board.fen(), str(result.move)
+	result = result.bestmove.uci()
+	return board.fen(), str(result)[:4]
 
 # if __name__ == '__main__': 
 # 	main()
